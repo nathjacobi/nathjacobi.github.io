e49e6131845600d429a445d438cc0076ea475e34
Nathaniel Jacobi <njacobi@mines.edu>
2015-11-30 14:29:01 -0700

    There is now a basic countdown to my graduation

--- clock.js
+++ clock.js
@@ -1,6 +1,30 @@
-function myFunction() {
-    var d = new Date();
-    var n = d.getTime();
-    
-    document.getElementById("demo").innerHTML = n;
+var gradDate = new Date("2016-05-13T09:00:00");
+var gradTime = gradDate.getTime();
+var currentDate;
+var currentTime;
+var timeTo;
+var countDown = [];
+$(document).ready(function() {
+	$("button").click(function(){
+        $("#demo").hide();
+    });
+	window.setInterval(function(){
+		getCurrentTime();
+	}, 1000);});
+
+function getCurrentTime() {
+    currentDate = new Date();
+    currentTime = currentDate.getTime();
+    document.getElementById("Date").innerHTML = countDown;
+    calculateTimeTo();
+};
+
+function calculateTimeTo() {
+	timeTo = gradTime-currentTime;
+	countDown["Days"] = Math.floor(timeTo/(60*60*1000*24)*1);;
+	countDown["Hours"] = Math.floor((timeTo%(60*60*1000*24))/(60*60*1000)*1);;
+	countDown["Minutes"] = Math.floor(((timeTo%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);;
+	countDown["Seconds"] = Math.floor((((timeTo%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);;
+	document.getElementById("Date").innerHTML = countDown["Days"] + " Days " + countDown["Hours"] + " Hours " + 
+		countDown["Minutes"] + " Minutes " + countDown["Seconds"] + " Seconds";
 }
